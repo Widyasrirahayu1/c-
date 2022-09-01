26ee5aa5b2838ec18be07b57b5c4f01133ad3c27
Widyasrirahayu1 <111963197+Widyasrirahayu1@users.noreply.github.com>
2022-09-01 15:41:19 +0700

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+//Compiler version g++ 6.3.0
+int main()
+{
+ cout << "Nama:Widya"<< endl;
+ cout << "Kelas:xpplg1"<< endl;
+ cout << "=========="<< endl;
+ cout << "Angka 1 = 10 " << endl;
+ cout << "Angka 2 = 5 "<< endl;
+ cout << "Hasil : " << 10+5 << endl;
+ 
+return 0 ;
+}
